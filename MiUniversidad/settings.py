db56812f508422d815b91e2aae23fa230983f329
ysaiascampos <ysaiascampos@gmail.com>
2021-04-19 20:33:24 -0400

    Update settings.py

--- MiUniversidad/settings.py
+++ MiUniversidad/settings.py
@@ -129,5 +129,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_POST = 587
-EMAIL_HOST_USER = "ysaiascampos@gmail.com"
+EMAIL_HOST_USER = "ysaiasperezauto@gmail.com"
 EMAIL_HOST_PASSWORD = ""
